15aa92a5896fae2fa9dff65462b6355376e8d441
Jia Jun Lee <lee19@live.cn>
2023-10-01 11:44:27 +0800

    Update id into primaryID

--- app/_components/form-delete.js
+++ app/_components/form-delete.js
@@ -5,7 +5,7 @@ import SubmitButton from '../_components/button-submit';
 function FormDelete( {deleteUrl, primaryKey, primaryID, onDeleteClick} ) {
     return (
         <form action={deleteUrl}>
-            <input type="hidden" id={primaryKey} name={primaryKey} value={primaryID} required readOnly/>
+            <input type="hidden" id={primaryID} name={primaryKey} value={primaryID} required readOnly/>
             <SubmitButton buttonClass="button" buttonTitle="delete" onButtonClick={onDeleteClick} buttonPendingTitle="deleting"/>
         </form>
     );
