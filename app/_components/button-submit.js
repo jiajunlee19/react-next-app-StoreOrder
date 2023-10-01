d05b8c505659d800b7e2a6ae5b327bc623e24c4d
Jia Jun Lee <lee19@live.cn>
2023-10-01 10:58:33 +0800

    Add pending status

--- app/_components/button-submit.js
+++ app/_components/button-submit.js
@@ -4,10 +4,10 @@ import {experimental_useFormStatus as useFormStatus} from 'react-dom'
 function SubmitButton( {buttonClass, buttonTitle, onButtonClick} ) {
 
     // useFormStatus gives a boolean, use this to tell if the button should be aria-disabled or not
-    const {isFormPending} = useFormStatus();
+    const { pending } = useFormStatus();
 
     return (
-        <button className={buttonClass} type="submit" aria-disabled={isFormPending} onClick={onButtonClick}>
+        <button className={buttonClass} type="submit" aria-disabled={pending} onClick={onButtonClick}>
             {buttonTitle}
         </button>
     );
