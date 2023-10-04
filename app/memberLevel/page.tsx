f45e8ec6bfa0cb975e10e7da7c0b8412d9fba276
Jia Jun Lee <lee19@live.cn>
2023-10-04 19:35:32 +0800

    Add memberLevel page

---
+++ app/memberLevel/page.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import HeaderLeft from '@/app/_components/page/header-left';
+import HeaderMid from '@/app/_components/page/header-mid';
+import HeaderRight from '@/app/_components/page/header-right';
+import Main from '@/app/_components/page/main';
+import { getMemberLevel, insertMemberLevel, updateMemberLevel, deleteMemberLevel } from '@/app/_actions/memberLevel';
+
+// Define staticSiteRendering function
+async function MemberLevel () {
+
+    // fetch data
+    const fetchedData = await getMemberLevel();
+
+    // declare all input types for inputDictInsert
+    const inputDictInsert = {
+        'member_level_name': 'text',
+        'bonus_points_min': 'number',
+        'bonus_points_max': 'number'
+    };
+
+    // declare all input types for inputDictUpdate
+    const inputDictUpdate = {
+        'member_level_id': 'hidden',
+        'member_level_name': 'readonly',
+        'bonus_points_min': 'number',
+        'bonus_points_max': 'number'
+    };    
+
+    // declare primary key
+    const primaryKey = 'member_level_id';
+
+    // declare column list to be displayed, this must be a subset of fetched data columns
+    const columnListDisplay = ['member_level_name', 'bonus_points_min', 'bonus_points_max']
+
+    // declare insert/update/delete async function
+    const insertAction = insertMemberLevel;
+    const updateAction = updateMemberLevel;
+    const deleteAction = deleteMemberLevel;
+
+    return (
+      <React.Fragment>
+        <header className='header'>
+          <HeaderLeft />
+          <HeaderMid />
+          <HeaderRight />
+        </header>
+        <main className='main'>
+            <Main headerTitle="Manage Member Level" fetchedData={fetchedData} inputDictInsert={inputDictInsert} inputDictUpdate={inputDictUpdate} primaryKey={primaryKey} columnListDisplay={columnListDisplay} insertAction={insertAction} updateAction={updateAction} deleteAction={deleteAction} />
+        </main>
+      </React.Fragment>
+    );
+  };
+
+export default MemberLevel;
