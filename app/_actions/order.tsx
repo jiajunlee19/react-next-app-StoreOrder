afda5249e599aef975a59505671d65f723ab1b5f
Jia Jun Lee <lee19@live.cn>
2023-10-08 14:21:27 +0800

    Create order route (test)

---
+++ app/_actions/order.tsx
@@ -0,0 +1,175 @@
+'use server'
+
+import dotenv from 'dotenv';
+import { v5 as uuidv5 } from 'uuid';
+import {z} from 'zod';
+import { revalidatePath } from 'next/cache';
+import prisma from '@/prisma/prisma';
+
+export async function getMember() {
+
+    try {
+
+        // get result from prisma
+        const result = await prisma.member.findMany({
+            select: {
+                member_id: true,
+                member_name: true
+            }
+        });
+
+        // Check existing cache, revalidate with the fetched data
+        revalidatePath('/');
+
+        return result
+
+    } catch(e) {
+        return []
+    }
+
+};
+
+export async function getOrder() {
+
+    try {
+
+        // get result from prisma
+        const result = await prisma.$queryRaw`
+            SELECT o.*, m.member_name 
+            FROM "order" o 
+            inner join member m on o.member_id = m.member_id
+        `;
+        // const result = await prisma.order.findMany({
+
+        // });
+
+        // Check existing cache, revalidate with the fetched data
+        revalidatePath('/');
+
+        return result
+
+    } catch(e) {
+        return []
+    }
+
+};
+
+export async function insertOrder(prevState: any, formData: FormData) {
+
+    // Retrive environment variables
+    dotenv.config({ path: '@/env/.env' });
+    const UUID5_NAMESPACE = process.env.UUID5_NAMESPACE;
+    const UUID5_SECRET = uuidv5(UUID5_NAMESPACE, uuidv5.DNS)
+    const UUID5_DELIMITER = process.env.UUID5_DELIMITER;
+
+    // Set current datetime
+    const now = new Date();
+
+    // set zod schema to validate form data
+    const schema = z.object({
+        order_id: z.string().uuid(),
+        member_id: z.string().uuid(),
+        order_created_date: z.date(),
+        order_updated_date: z.date()
+    });
+    const data = schema.parse({
+        order_id: uuidv5(formData.get('member_id') + (UUID5_DELIMITER || '|') + now.toString, UUID5_SECRET),
+        member_id: formData.get('member_id'),
+        order_created_date: now,
+        order_updated_date: now
+    });
+
+    try {
+
+        // get result from prisma
+        const result = await prisma.order.create({
+            data: data,
+        });
+
+        // Check existing cache, revalidate with the fetched data
+        revalidatePath('/');
+
+
+        return { message: `Successfully inserted ${data['order_id']}` }
+
+    } catch(e) {
+        return { message: 'Failed to insert the item' }
+    }
+
+};
+
+export async function updateOrder(prevState: any, formData: FormData) {
+
+    // Retrive environment variables
+    dotenv.config({ path: '@/env/.env' });
+    const UUID5_NAMESPACE = process.env.UUID5_NAMESPACE;
+    const UUID5_SECRET = uuidv5(UUID5_NAMESPACE, uuidv5.DNS)
+    const UUID5_DELIMITER = process.env.UUID5_DELIMITER;
+
+    // Set current datetime
+    const now = new Date();
+
+    // set zod schema to validate form data
+    const schema = z.object({
+        order_id: z.string().uuid(),
+        member_id: z.string().uuid(),
+        order_updated_date: z.date()
+    });
+    const data = schema.parse({
+        order_id: formData.get('order_id'),
+        member_id: formData.get('member_id'),
+        order_updated_date: now
+    });
+
+    try {
+
+        // get result from prisma
+        const result = await prisma.order.update({
+            where: {
+                order_id: data['order_id']
+            },
+
+            data: data,
+        });
+
+        // Check existing cache, revalidate with the fetched data
+        revalidatePath('/');
+
+
+        return { message: `Successfully updated ${data['order_id']}` }
+
+    } catch(e) {
+        return { message: 'Failed to update the item' }
+    }
+
+};
+
+export async function deleteOrder(prevState: any, formData: FormData) {
+
+    // set zod schema to validate form data
+    const schema = z.object({
+        order_id: z.string().uuid()
+    });
+    const data = schema.parse({
+        order_id: formData.get('order_id')
+    });
+
+    try {
+
+        // get result from prisma
+        const result = await prisma.order.delete({
+            where: {
+                order_id: data['order_id']
+            },
+        });
+
+        // Check existing cache, revalidate with the fetched data
+        revalidatePath('/');
+
+        return { message: `Successfully deleted ${data['order_id']}` }
+
+    } catch(e) {
+        return { message: 'Failed to delete the item' }
+    }
+
+};
---
+++ app/page.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import HeaderLeft from '@/app/_components/page/header-left';
+import HeaderMid from '@/app/_components/page/header-mid';
+import HeaderRight from '@/app/_components/page/header-right';
+import Main from '@/app/_components/page/main';
+import { getMember, getOrder, insertOrder, updateOrder, deleteOrder } from '@/app/_actions/order';
+
+// Define staticSiteRendering function
+async function Order () {
+
+    // fetch data
+    const fetchedData = await getOrder();
+    // const d = fetchedData[0]['order_created_date'];
+    // const a = Object.fromEntries([['date', d]])
+    // console.log(a);
+
+    // declare all input types for inputDictInsert
+    const inputDictInsert = {
+        'member_id': 'dynamic',
+        'member_name': 'select'
+    };
+
+    // declare all input types for inputDictUpdate
+    const inputDictUpdate = {
+        'order_id': 'readonly',
+        'member_id': 'readonly',
+        'member_name': 'readonly'
+    };    
+
+    // declare primary key
+    const primaryKey = 'order_id';
+
+    // declare column list to be displayed, this must be a subset of fetched data columns
+    const columnListDisplay = ['order_id', 'member_name',]
+    // 'order_created_date', 'order_updated_date'
+
+    // declare insert/update/delete async function
+    const insertAction = insertOrder;
+    const updateAction = updateOrder;
+    const deleteAction = deleteOrder;
+
+    // fetch data for select options
+    const selectOptionData = await getMember();
+    const selectPrimaryKey = 'member_id';
+    const selectPrimaryKeyList: string[] = ['member_name'];
+
+    return (
+      <React.Fragment>
+        <header className='header'>
+          <HeaderLeft />
+          <HeaderMid />
+          <HeaderRight />
+        </header>
+        <main className='main'>
+            <Main headerTitle="Manage Order" formTitle="Order" fetchedData={fetchedData} inputDictInsert={inputDictInsert} inputDictUpdate={inputDictUpdate} primaryKey={primaryKey} columnListDisplay={columnListDisplay} insertAction={insertAction} updateAction={updateAction} deleteAction={deleteAction} selectOptionData={selectOptionData} selectPrimaryKey={selectPrimaryKey} selectPrimaryKeyList={selectPrimaryKeyList} />
+        </main>
+      </React.Fragment>
+    );
+  };
+
+export default Order;
