337ca7fdac60fa05e05b69caf1c65b8684b625bd
Jia Jun Lee <lee19@live.cn>
2023-10-14 08:23:18 +0800

    Infer all CRUD types from zod

--- app/_libs/types.ts
+++ app/_libs/types.ts
@@ -1,14 +1,39 @@
 import { z } from "zod";
-import { MemberSchema, MemberLevelSchema, UomSchema, ProductSchema, OrderSchema, OrderItemSchema } from "./zod-form-server";
+import { 
+    SelectMemberSchema, InsertMemberSchema, UpdateMemberSchema, DeleteMemberSchema,
+    SelectMemberLevelSchema, InsertMemberLevelSchema, UpdateMemberLevelSchema, DeleteMemberLevelSchema,
+    SelectUomSchema, InsertUomSchema, UpdateUomSchema, DeleteUomSchema,
+    SelectProductSchema, InsertProductSchema, UpdateProductSchema, DeleteProductSchema,
+    SelectOrderSchema, InsertOrderSchema, UpdateOrderSchema, DeleteOrderSchema,
+    SelectOrderItemSchema, InsertOrderItemSchema, UpdateOrderItemSchema, DeleteOrderItemSchema
+} from "./zod-form-server";
 
-export type Member = z.infer<typeof MemberSchema>;
+export type TSelectMember = z.infer<typeof SelectMemberSchema>;
+export type TInsertMember = z.infer<typeof InsertMemberSchema>;
+export type TUpdateMember = z.infer<typeof UpdateMemberSchema>;
+export type TDeleteMember = z.infer<typeof DeleteMemberSchema>;
 
-export type MemberLevel = z.infer<typeof MemberLevelSchema>;
+export type TSelectMemberLevel = z.infer<typeof SelectMemberLevelSchema>;
+export type TInsertMemberLevel = z.infer<typeof InsertMemberLevelSchema>;
+export type TUpdateMemberLevel = z.infer<typeof UpdateMemberLevelSchema>;
+export type TDeleteMemberLevel = z.infer<typeof DeleteMemberLevelSchema>;
 
-export type Uom = z.infer<typeof UomSchema>;
+export type TSelectUom = z.infer<typeof SelectUomSchema>;
+export type TInsertUom = z.infer<typeof InsertUomSchema>;
+export type TUpdateUom = z.infer<typeof UpdateUomSchema>;
+export type TDeleteUom = z.infer<typeof DeleteUomSchema>;
 
-export type Product = z.infer<typeof ProductSchema>;
+export type TSelectProduct = z.infer<typeof SelectProductSchema>;
+export type TInsertProduct = z.infer<typeof InsertProductSchema>;
+export type TUpdateProduct = z.infer<typeof UpdateProductSchema>;
+export type TDeleteProduct = z.infer<typeof DeleteProductSchema>;
 
-export type Order = z.infer<typeof OrderSchema>;
+export type TSelectOrder = z.infer<typeof SelectOrderSchema>;
+export type TInsertOrder = z.infer<typeof InsertOrderSchema>;
+export type TUpdateOrder = z.infer<typeof UpdateOrderSchema>;
+export type TDeleteOrder = z.infer<typeof DeleteOrderSchema>;
 
-export type OrderItem = z.infer<typeof OrderItemSchema>;
+export type TSelectOrderItem = z.infer<typeof SelectOrderItemSchema>;
+export type TInsertOrderItem = z.infer<typeof InsertOrderItemSchema>;
+export type TUpdateOrderItem = z.infer<typeof UpdateOrderItemSchema>;
+export type TDeleteOrderItem = z.infer<typeof DeleteOrderItemSchema>;
